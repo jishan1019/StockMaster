5e885e97c35b70273032cf4b202be046406285b4
jishan1019 <imranislamjishan200@gmail.com>
2023-10-25 01:25:09 +0600

    home page basic design add

--- index.php
+++ index.php
@@ -1,13 +1,52 @@
+<?php
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StockMaster</title>
+    <title> Home - StockMaster</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+    <!-- Css Link -->
+    <link rel="stylesheet" href="css/index.css">
+
+
+
 </head>
 
 <body>
+
+    <head>
+        <h1 class="align_text">Wellcome to StockMaster.</h1>
+    </head>
+
+    <main>
+        <div class="col-sm-6">
+            <a href="login.php">
+                <input type="button" class="btn btn-danger pull-right" value="Login Now">
+            </a>
+        </div>
+
+        <div class="col-sm-6">
+            <a href="signup.php">
+                <input type="button" class="btn btn-success pull-left" value="Signup Now">
+            </a>
+        </div>
+    </main>
 
 </body>
 
