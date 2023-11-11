244a0dce7b462ed49e081b8ea106c5fffbd7af77
jishan1019 <imranislamjishan200@gmail.com>
2023-11-11 12:34:27 +0600

    login from basic design

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Now</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Css Link -->
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <form method="POST" action="">
+
+        <!-- card container -->
+        <div class="cardContainer">
+            <div class="cardBody">
+                <h1>Log In </h1>
+
+                <div>
+                    <input type="email" name="email" id="email" placeholder="Enter Email">
+                </div>
+            </div>
+        </div>
+
+    </form>
+
+</body>
+
+</html>
